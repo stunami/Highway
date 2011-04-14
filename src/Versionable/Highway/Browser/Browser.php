9805ce20bef103f58e41e2f30115313717d37fb6
Stuart Lowes <stuart.lowes@gmail.com>
2011-04-15 00:17:26 +0100

    Adding basic methods to Browser
     - get, post, put, delete
     - back, forward, reload, go

--- src/Versionable/Highway/Browser/Browser.php
+++ src/Versionable/Highway/Browser/Browser.php
@@ -8,12 +8,16 @@ use Versionable\Prospect\Client\ClientInterface;
 use Versionable\Prospect\Request\RequestInterface;
 use Versionable\Prospect\Response\ResponseInterface;
 
+use Versionable\Highway\History\Entry;
+
 class Browser implements BrowserInterface
 {
   protected $client = null;
   
   protected $history = null;
   
+  protected $session = null;
+
   public function __construct(ClientInterface $client, HistoryInterface $history)
   {
     $this->setClient($client);
@@ -22,9 +26,66 @@ class Browser implements BrowserInterface
   
   public function get(RequestInterface $request, ResponseInterface $response)
   {
+    $request->setMethod('GET');
+    
+    return $this->callAndAddToHistory($request, $response);
+  }
+  
+  public function post(RequestInterface $request, ResponseInterface $response)
+  {
+    $request->setMethod('POST');
+    
+    return $this->callAndAddToHistory($request, $response);
+  }
+  
+  public function put(RequestInterface $request, ResponseInterface $response)
+  {
+    $request->setMethod('PUT');
+    
+    return $this->callAndAddToHistory($request, $response);
+  }
+  
+  public function delete(RequestInterface $request, ResponseInterface $response)
+  {
+    $request->setMethod('DELETE');
+    
+    return $this->callAndAddToHistory($request, $response);
+  }
+  
+  public function call(RequestInterface $request, ResponseInterface $response)
+  {   
+    echo $request->getUrl()->toString() . "\n";
+    
     return $this->getClient()->send($request, $response);
   }
   
+  public function back()
+  {
+    $entry = $this->getHistory()->back();
+    
+    return $this->call($entry->getRequest(), $this->getNewResonseObject($entry->getResponse()));
+  }
+  
+  public function forward()
+  {
+    $entry = $this->getHistory()->forward();
+    
+    return $this->call($entry->getRequest(), $this->getNewResonseObject($entry->getResponse()));
+  }
+  
+  public function go($relative)
+  {
+    $entry = $this->getHistory()->go($relative);
+    
+    return $this->call($entry->getRequest(), $this->getNewResonseObject($entry->getResponse()));
+  }
+  
+  public function reload()
+  {
+    return $this->go(0);
+  }
+
+
   public function getClient()
   {
     return $this->client;
@@ -43,5 +104,29 @@ class Browser implements BrowserInterface
   public function setHistory(HistoryInterface $history)
   {
     $this->history = $history;
+  }
+  
+  /*public function getSession()
+  {
+    return $this->session;
+  }
+
+  public function setSession($session) 
+  {
+    $this->session = $session;
+  }*/
+  
+  protected function callAndAddToHistory(RequestInterface $request, ResponseInterface $response)
+  {
+    $this->history->add(new Entry($request, $response));
+    
+    return $this->call($request, $response);    
+  }
+  
+  protected function getNewResonseObject(ResponseInterface $response)
+  {
+    $class = \get_class($response);
+    
+    return new $class();
   }
 }
--- src/Versionable/Highway/Browser/BrowserInterface.php
+++ src/Versionable/Highway/Browser/BrowserInterface.php
@@ -2,7 +2,40 @@
 
 namespace Versionable\Highway\Browser;
 
+use Versionable\Prospect\Client\ClientInterface;
+use Versionable\Prospect\Request\RequestInterface;
+use Versionable\Prospect\Response\ResponseInterface;
+use Versionable\Highway\History\HistoryInterface;
+
 interface BrowserInterface
 {
+  public function get(RequestInterface $request, ResponseInterface $response);
   
+  public function post(RequestInterface $request, ResponseInterface $response);
+  
+  public function put(RequestInterface $request, ResponseInterface $response);
+  
+  public function delete(RequestInterface $request, ResponseInterface $response);
+  
+  public function call(RequestInterface $request, ResponseInterface $response);
+  
+  public function back();
+  
+  public function forward();
+  
+  public function go($relative);
+  
+  public function reload();
+  
+  public function getClient();
+  
+  public function setClient(ClientInterface $client);
+  
+  public function getHistory();
+
+  public function setHistory(HistoryInterface $history);
+  
+  /*public function getSession();
+  
+  public function setSession();*/
 }
